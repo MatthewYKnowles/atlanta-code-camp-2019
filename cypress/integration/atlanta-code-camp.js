3496cadfd58f1edde09933ab0b5bed63da629353
Matthew Knowles <matthewyknowles@gmail.com>
2019-08-30 18:02:39 -0400

    initial commit

---
+++ cypress/integration/atlanta-code-camp.js
@@ -0,0 +1,8 @@
+describe('Atlanta Code Camp Website Tests', ()=> {
+    it('should show Matthew Knowles presenting in room 109', ()=> {
+        cy.visit('https://www.atlantacodecamp.com/2019');
+        cy.get('.navbar-collapse > :nth-child(2) > :nth-child(6) > a').click();
+        cy.contains('Matthew Knowles').click({force: true});
+        cy.get('.panel-body > .dl-horizontal > :nth-child(6) > a').contains(109)
+    })
+});
